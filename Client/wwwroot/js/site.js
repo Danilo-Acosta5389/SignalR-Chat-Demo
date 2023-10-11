7e124414510eb9c41d9a4c74c0a5b44ba27e8410
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-10-11 14:18:16 +0200

    first commit

---
+++ Client/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿//$(function () {
+//    $("#txtMessage").emojioneArea();
+//}); 
+
+//$('#InputField').append($div);
+//var msgTextbox = $div.find("#txtPrivateMessage");
+//$(msgTextbox).emojioneArea();  
+
+//function ParseEmoji(div) {
+//    var input = $(div).html();
+//    var output = emojione.unicodeToImage(input);
+//    $(div).html(output);
+//} 
